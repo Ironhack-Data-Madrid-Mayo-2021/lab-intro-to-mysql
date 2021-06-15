710b51a0efd78dd597a3d2206c097b5289b1716d
root <root@DESKTOP-H6FOBO7.localdomain>
2021-06-15 22:57:49 +0200

    modificaciones

---
+++ your-code/create.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE lab_mysql;
+USE lab_mysql;
+CREATE TABLE cars ( Vin INT, manufacturer VARCHAR(45), MODEL VARCHAR(45), YEAR DATE);
+CREATE TABLE customers ( idCustomers INT, name VARCHAR(45), phone_number VARCHAR(45), email VARCHAR(45);
+CREATE TABLE salesperson ( staff ID INT, name VARCHAR(45), store_of_the_company VARCHAR(45);
+CREATE TABLE invoices ( invoice_number INT, date DATE, car VARCHAR(45), customer VARCHAR(45), sales_person_of_car VARCHAR(45);
---
+++ your-code/seeding.sql
@@ -0,0 +1,15 @@
+INSERT INTO cars;
+( vin, manufacturer, model, year);
+values( [ 3K096I98581DHSNUP, ZM8G7BEUQZ97IH46V, RKXVNNIHLVVZOUB4M, HKNDGS7CU31E9Z7JW, DAM41UDN3CHU2WVF6 ], [ Volkswagen, peugeot, Ford, Toyota, Volvo ], [ Tiguan, Rifter, Fusion, RAV4, V60 ], [ 2019, 2019, 2018, 2018, 2019 ], [ Blue, Red, White, Silver, Gray ]
+INSERT INTO customers;
+( idcustomers, name, phone_number, email )
+values ( [ 10001, 20001, 30001 ], [ Pablo_Picasso, Abraham_Lincoln, Napoléon_Bonaparte ], [], [ Paseo_de_la_chopera14, 120_SW_8th_St, 40_Rue_du_colisee ], [ Madrid, Miami, Paris ], [ Madrid, Florida, Ile-de-France ], [ Spain, United_states, France ], [ 28045, 33130, 75008 ];
+
+INSERT Into salespersons;
+( staff_ID, name, store_of_the_company )
+values ( [ 00001, 00002, 00003, 00004, 00005, 00006, 00007, 00008 ], [ Petey_Cruiser, Anna_Sthesia, Paul_Molive, Gail_Forecewind, Paige_Turner, Bob_Frapples, Walter_Melon, Shonda_leer ], [ Madrid, Barcelona, Berlin, Paris, Miami, Mexico_city, Amsterdam, Sao_Paulo ];
+
+INSERT INTO invoies;
+( Invoice_number, date, car, customer, sales_person_of_car );
+values ( [ 852399038, 731166526, 271135104 ], [ 22-08-2018, 31-12-2018, 22-01-2019 ], [ 0, 3, 2 ], [ 1, 0, 2 ], [ 3, 5, 7 ];
+
